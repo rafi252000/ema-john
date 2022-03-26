4867c2732e570fc6a76a4391e0cdbf4ee1dfa8aa
rafi252000 <rafiuddin.cse.pciu@gmail.com>
2022-03-26 12:48:15 +0600

    added code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import Shope from './components/Shop/Shope';
 
 function App() {
   return (
     <div >
       <Header></Header>
-      
+      <Shope></Shope>
     </div>
   );
 }
